fe5bc786ce0c16d08741bb9eac5a6364e066a801
hassanSayed0 <111157516+hassanSayed0@users.noreply.github.com>
2023-02-08 21:05:00 +0200

    done segue formatted

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -20,7 +20,7 @@ class RegisterViewController: UIViewController {
                 if let err = error{
                     print(err.localizedDescription)
                 }else{
-                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
                 }
             }
         }
